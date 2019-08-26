866ddf0ea25c01cdbf06ac97af817f5370a57138
Pedro De Ona <pdeona@rbi.com>
2019-08-26 11:30:52 -0400

    update type definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,9 +6,116 @@ declare module '@capacitor/core' {
 
 export interface RadarCallback {
   status: string;
-  location: any;
-  user: any;
-  events: any;
+  location: NativeLocation;
+  user: RadarUser;
+  events: RadarEvent[];
+}
+
+interface Chain {
+  name: string;
+  slug: string;
+}
+
+interface CodedLocation {
+  _id: string;
+  code: number;
+  name: string;
+  type: string;
+}
+
+type Country = CodedLocation;
+
+type DMA = CodedLocation;
+
+interface Geofence {
+  _id: string;
+  externalId: string;
+  geometry: NativeLocation;
+  tag: string;
+  metadata: {
+    [key: string]: string;
+  };
+}
+
+interface Insights {
+  homeLocation: Nullable<Location>;
+  officeLocation: Nullable<Location>;
+  state: {
+    home: null | boolean;
+    office: null | boolean;
+    traveling: null | boolean;
+  };
+}
+
+interface Location {
+  confidence: RadarEventConfidence;
+  location: NativeLocation;
+  type: string;
+}
+
+interface NativeLocation {
+  accuracy: number;
+  latitude: number;
+  longitude: number;
+}
+
+interface Place {
+  _id: string;
+  categories: string[];
+  chain: Nullable<Chain>;
+  location?: Nullable<Location>;
+  name: string;
+}
+
+type PostalCode = CodedLocation;
+
+enum RadarEventConfidence {
+  none = 0,
+  low = 1,
+  medium = 2,
+  high = 3
+}
+
+interface RadarEvent {
+  _id: string;
+  actualCreatedAt: string;
+  alternatePlaces: null | Place;
+  confidence: RadarEventConfidence;
+  createdAt: string;
+  duration: number;
+  geofence: null | Geofence;
+  live: boolean;
+  place: null | Place;
+  region: null | Region;
+  type: RadarEventType;
+  verification: RadarEventVerification;
+  verifiedPlace: null | Place;
+}
+
+type RadarEventType =
+  | 'unknown'
+  | 'user.entered_geofence'
+  | 'user.entered_home'
+  | 'user.entered_office'
+  | 'user.entered_place'
+  | 'user.entered_region_country'
+  | 'user.entered_region_dma'
+  | 'user.entered_region_state'
+  | 'user.exited_geofence'
+  | 'user.exited_home'
+  | 'user.exited_office'
+  | 'user.exited_place'
+  | 'user.exited_region_country'
+  | 'user.exited_region_dma'
+  | 'user.exited_region_state'
+  | 'user.nearby_place_chain'
+  | 'user.started_traveling'
+  | 'user.stopped_traveling';
+
+enum RadarEventVerification {
+  accept = 1,
+  unverify = 0,
+  reject = -1
 }
 
 export interface RadarLocationPermissionsCallback {
@@ -32,3 +139,32 @@ export interface RadarPlugin {
   acceptEvent(options: { eventId: string, verifiedPlaceId: string }): void;
   rejectEvent(options: { eventId: string }): void;
 }
+
+interface RadarUser {
+  _id: string;
+  country: Nullable<Country>;
+  dma: Nullable<DMA>;
+  geofences: Nullable<Geofence>[];
+  insights: Insights;
+  location: Nullable<NativeLocation>;
+  place: Nullable<Place>;
+  postalCode: Nullable<PostalCode>;
+  state: Nullable<State>;
+  userId: null | string;
+}
+
+interface Region {
+  country: Nullable<Country>;
+  dma: Nullable<DMA>;
+  postalCode: Nullable<PostalCode>;
+  state: Nullable<State>;
+}
+
+type State = CodedLocation;
+
+/**
+ * Recursive nullable object type
+ */
+type Nullable<Props extends object> = {
+  [K in keyof Props]: Props[K] extends object ? Nullable<Props[K]> : Props[K] | null
+};
