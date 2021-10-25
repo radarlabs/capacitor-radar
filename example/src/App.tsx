d3ce0671e0f50832ee064bdc1278c67ce0943ede
Nick Patrick <nicholasjohnpatrick@gmail.com>
2021-10-24 21:51:07 -0400

    ci

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
-// import { Radar } from 'capacitor-radar';
+import { Radar } from 'capacitor-radar';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
