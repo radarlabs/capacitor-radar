be468219b679daad57f593ad4bebb7ed8e3489d7
Nick Patrick <nicholasjohnpatrick@gmail.com>
2021-10-25 14:17:19 -0400

    add getContext()

--- android/src/main/java/io/radar/capacitor/RadarPlugin.java
+++ android/src/main/java/io/radar/capacitor/RadarPlugin.java
@@ -109,7 +109,7 @@ public class RadarPlugin extends Plugin {
             }
         };
 
-        registerReceiver(receiver, filter);
+        getContext().getApplicationContext().registerReceiver(receiver, filter);
     }
 
     @PluginMethod()
