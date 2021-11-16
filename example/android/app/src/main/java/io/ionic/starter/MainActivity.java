fad7217751d4e7bb40c9d75a494a83cc185381ba
phil-brown <philip22brown@gmail.com>
2021-11-16 10:25:36 -0600

    Update to Radar Android SDK 3.2.2

--- example/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ example/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 
-import io.radar.sdk.Radar;
+import io.radar.capacitor.RadarPlugin;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Radar.initialize(this, "prj_test_pk_0000000000000000000000000000000000000000");
+        //loads plugin and initializes Radar using the publishable key in strings.xml
+        registerPlugin(RadarPlugin.class);
     }
 
 }
