1b29ea7912af696e6b5382630f48c0cfa00eb67f
Nick Patrick <nicholasjohnpatrick@gmail.com>
2022-03-29 01:36:29 -0400

    fix errors

--- android/src/main/java/io/radar/capacitor/RadarPlugin.java
+++ android/src/main/java/io/radar/capacitor/RadarPlugin.java
@@ -22,6 +22,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
