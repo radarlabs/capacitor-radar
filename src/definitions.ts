085e87e4ecaca140f3a5b2b9fe857194e30f8e73
Kenny Hu <kenny.hu@radar.com>
2023-08-07 16:20:39 -0400

    changed interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -40,6 +40,7 @@ export interface RadarPlugin {
   searchPlaces(options: { near?: Location, radius: number, chains?: string[], chainMetadata?: object, categories?: string[], groups?: string[], limit: number }): Promise<RadarSearchPlacesCallback>;
   searchGeofences(options: { near?: Location, radius: number, metadata?: object, tags?: string[], limit: number }): Promise<RadarSearchGeofencesCallback>;
   autocomplete(options: { query: string, near?: Location, layers?: string[], limit: number, country?: string, expandUnits?: boolean }): Promise<RadarGeocodeCallback>;
+  autocomplete(options: { query: string, near?: Location, layers?: string[], limit: number, countryCode?: string, expandUnits?: boolean }): Promise<RadarGeocodeCallback>;
   validateAddress(options: { address: RadarAddress }): Promise<RadarValidateAddressCallback>;
   geocode(options: { query: string }): Promise<RadarGeocodeCallback>;
   reverseGeocode(options?: Location): Promise<RadarGeocodeCallback>;
