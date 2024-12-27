314b607080ae8b6993ddc11fa0985290a0322658
Nick Patrick <nicholasjohnpatrick@gmail.com>
2024-12-27 14:36:42 -0500

    bump versions

--- android/src/main/java/io/radar/capacitor/RadarPlugin.java
+++ android/src/main/java/io/radar/capacitor/RadarPlugin.java
@@ -172,7 +172,7 @@ public class RadarPlugin extends Plugin {
         String publishableKey = call.getString("publishableKey");
         SharedPreferences.Editor editor = this.getContext().getSharedPreferences("RadarSDK", Context.MODE_PRIVATE).edit();
         editor.putString("x_platform_sdk_type", "Capacitor");
-        editor.putString("x_platform_sdk_version", "3.14.0");
+        editor.putString("x_platform_sdk_version", "3.14.2-beta.1");
         editor.apply();
         Radar.initialize(this.getContext(), publishableKey);
         call.resolve();
