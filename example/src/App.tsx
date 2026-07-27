f2fd7a3bd4fe2653f8b681603511d117fe3e403d
Alan <alan.charles@radar.com>
2026-07-27 14:35:35 -0700

    cleanup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
   useEffect(() => {
     StatusBar.setOverlaysWebView({ overlay: false });
 
-    Radar.initialize({'publishableKey': 'prj_test_pk_7b46891aa0a8278b5acc6bbc9f227aa5c3319483'});
+    Radar.initialize({'publishableKey': 'prj_test_pk_000000000000000000000000000000000000'});
     Radar.setLogLevel({ level: 'debug' });
     Radar.setUserId({ userId: 'capacitor_test'});
     Radar.setDescription({ description: 'capacitor example' });
